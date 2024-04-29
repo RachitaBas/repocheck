4e10d428a984d379c50c18c31d42bc35e0b59cfd
Rashita Basnet <rbasnet@infinite.com>
2024-04-29 13:28:01 +0545

    first commit

---
+++ projects/demofirst.py
@@ -0,0 +1,288 @@
+# string i = "hello"
+# string j = "kerala"
+# int bk = stringi + stringj
+# print(intk)
+
+"""The division rule """
+
+# num1 = 2
+# num2 = 7
+# multiply= num1/num2
+# print("The division is:",multiply)
+
+# x = "ramesh"
+# y = "suresh"
+# print(x)
+# print(y)
+
+# a = 5  
+# print("The type of a,type(a)")  
+  
+# list2=[1,"rach","newone"]
+# print(type(list2))
+
+# list1  = [1, "hi", "Python", 2]    
+# #Checking type of given list  
+# print(type(list1)) 
+
+ 
+# Tuples
+# def divide(dividend,divisor):
+#     quotient = dividend // divisor
+#     remainder = dividend % divisor
+#     return quotient,remainder
+
+# result = divide(4,2)
+# print(result)
+
+#list
+# list = [2,3,"hello",[2,3]]
+# print(list)
+
+#indexing in list 
+# create_list=[2,3,"pooja",[2,3],"baneshor"]
+# list1 = [3, 5, 6, 1, 9]
+# inde= [0, 1, 2, 3, 4]
+# -ine= [-5,-4,-3, -2, -1]
+
+
+# print(create_list,create_list[2]) 
+# print(create_list,[0])
+# print(create_list,[4])
+# print(create,create_list[0:-2])
+# print(create,create_list[0:-3])
+# print(create,create_list[0:3:2])
+# print(create,create_list[:])
+# print(create,create_list[:2])
+# print(create,create_list[-1:])
+# print(create,create_list[::2])
+# print(create,create_list[::-3])
+
+# juice=["pineapple","coke","fanta"]  #Membership operator
+# print("coke" in juice)
+
+# x = 3  #Identity operator for same object id
+# y = 9
+# z = x
+# print(x is y)
+# print(y is x)
+# print(x is  not y)
+# print(id(x))  #object id for x
+# print(id(y))
+# print(id(z))
+
+# string1 = "rachita"
+# string2 = "rachita@!@#"
+# print(string1 is string2)
+
+# list1=(3,4,5,6)
+# list2=(3,4,5,6)
+# print(list1 is list2)
+
+# tuple1=(4,5,6)
+# tuple
+# # tuple2=(4,5,6)
+# print(id(tuple1))
+# print(tuple1 is tuple2)
+
+# x=()
+# y=()
+# print(x is y)
+
+# y = None  #assigning empty value to the variable 
+# z = None
+# print(y is z)
+
+# x = 2
+# y = 3
+# sum = x + y
+# print("The sum is",sum)
+
+# x = 2
+# y = 3
+# multiply = x * y
+# print("The multiplication is",multiply)
+
+# string1 = "rachita"
+# string2 = "basnet"
+# print(string1+" "+string2)
+
+# a = 2
+# b = 3
+# print(a+b)
+
+# x = 4
+# y = 4
+# modulus = x%y
+# print("The modulus is",modulus)
+
+# x = 2
+# y = 3
+# division = x/y
+# print("The division is",division)
+
+# a = 2
+# b = 2
+# print(a==b)
+
+# string1 = "rachita"
+# string2 = "basnet"
+# print(string1!=string2)
+
+# string1 = "rachita"
+# string2 = "basnet"
+# print(string1<=string2)
+
+# string1 = "rachita"
+# string2 = "basnet"
+# print(string1>=string2)
+
+# string1 = "rachita"
+# string2 = "basnet"
+# print(string1>string2)
+
+# string1 = "rachita"
+# string2 = "basnet"
+# print(string1<string2)
+
+# string2 = "rachitaa"
+# string3 = "rachitaf"
+# print(string2<string3)
+
+
+
+# a = 2
+# b = 2
+# print(a!=b)
+
+# a = 4
+# b = 6
+# print(a>=b)
+
+# a = 8
+# b = 9
+# print(a<=b)
+
+# a = 4
+# b = 0
+# print(a>b)
+
+# a = 2
+# b = 2
+# print(a<=b)
+
+# #python float
+# a = 2.3
+# b = 2.3
+# print(a + b)
+
+# b=4.5
+# c=2.3
+# print(b-c)
+
+# b=4.5
+# c=2.3
+# print(b*c)
+
+# b=4.5
+# c=2.3
+# print(b/c)
+
+# b=4.5
+# c=2.3
+# print(b==c)
+
+# b=4.5
+# c=2.3
+# print(b<=c)
+
+# b=4.5
+# c=2.3
+# print(b>=c)
+
+# b=4.5
+# c=2.3
+# print(b * c)
+
+# b=4.5
+# c=2.3
+# print(b-c)
+
+# b=4.5
+# c=2.3
+# print(b**c)   #for exponential
+
+# list_new = ["rach","hello","console"]
+# list_new.append("cyber")
+# print(list_new)
+
+# list_new = ["rach","hello","console"]
+# list_new.append("hello")
+# print(list_new)
+
+# list_new = ["rach","hello","console"]
+# transit=["roar","pass"]
+# list_new.extend("transit")
+# print(list_new)
+
+# list_new = [2,3,4]
+# transit=[7,8,9]
+# list_new.extend(transit)
+# print(list_new)
+
+# list_new = [2,3,4]
+# transit=["rach",8,9]
+# list_new.extend(transit)
+# print(list_new)
+
+# list_new.append("hello")
+# print(list_new)
+
+
+# juice=["pineapple","coke","fanta"]  #Membership operator
+# print("py"  not  in juice)
+
+# juice=["pineapple","coke","fanta"]  #Membership operator
+# print("py"  not  in juice)
+
+# juice=["pineapple","coke","fanta"]  #Membership operator
+# print("mango" not  in juice)
+
+# num_str = "abc"
+# try:
+#     num_int = int(num_str)
+#     print(num_int)
+# except ValueError:
+#     print("Invalid input. Cannot convert to integer.")
+
+# x = 5  # integer #dataconversion
+# y = 2 # float
+# result = x + y
+# print(result)
+
+# a = 6.7
+# b = 5
+# result = a >= b
+# print( "The result is",result)
+
+# int_number=2.3 #converting int to float number
+# float_number=2
+# result=float(int_number)
+# print(result)
+
+int_number=2 #converting int,string to float number and viceversa explicitly
+float_number=0.2
+int_number1="rach"
+string="984"
+result1=float(int_number)
+result2=int(float_number)
+result3=str(int_number1)
+result4=int(string)
+print(result1,result2,result3,result4)
+
+
+
+
+
+
+
